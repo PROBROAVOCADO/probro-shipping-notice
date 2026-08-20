304cdca1a9c76e36e794fc8e21ce4bdcc99ce309
Hsieh <mike96531@gmail.com>
2026-08-20 15:17:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* 波波酪梨 · 出貨通知系統  sw.js  v1.5.1
+/* 波波酪梨 · 出貨通知系統  sw.js  v1.7.0
  * 改版時務必更新 CACHE 名稱，否則舊快取不會被淘汰。
  */
-const CACHE = 'probro-ship-v1.5.1';
+const CACHE = 'probro-ship-v1.7.0';
 
 const ASSETS = [
   './', './index.html', './app.js', './manifest.json',
